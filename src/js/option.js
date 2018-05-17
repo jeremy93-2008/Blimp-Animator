c4bf85275f6c5f435c13021883325efa4887c5df
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2018-05-17 23:56:44 +0200

    Mejora diseño de la pantalla preferencias opciones

--- src/js/option.js
+++ src/js/option.js
@@ -2,7 +2,30 @@ var buildOption = {};
 document.querySelector(".BasicTab").addEventListener("click", function()
 {
 	this.classList.add("selected");
+	this.style.zIndex = 1;
 	document.querySelector(".AdvancedTab").classList.remove("selected");
+	document.querySelector(".AdvancedTab").style.zIndex = 2
 	document.querySelector(".BasicContainer").style.display = "block";
 	document.querySelector(".AdvancedContainer").style.display = "none";
 })
+document.querySelector(".AdvancedTab").addEventListener("click", function()
+{
+	this.classList.add("selected");
+	this.style.zIndex = 2;
+	document.querySelector(".BasicTab").style.zIndex = 1
+	document.querySelector(".BasicTab").classList.remove("selected");
+	document.querySelector(".AdvancedContainer").style.display = "block";
+	document.querySelector(".BasicContainer").style.display = "none";
+})
+let btn_list = document.querySelectorAll(".sidebarElm div")
+for(let btn of btn_list)
+{
+	btn.addEventListener("click", function()
+	{
+		for(let btn of btn_list)
+		{
+			btn.classList.remove("selected");
+		}
+		this.classList.add("selected");
+	})
+}
